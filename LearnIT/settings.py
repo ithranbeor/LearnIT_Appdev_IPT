4e288183250166961cfb908716041e982721a097
Ithran Beor Turno <turno.ithranbeor7@gmail.com>
2025-05-20 16:35:29 +0800

    Update email configuration in settings.py and requirements.txt

--- LearnIT/settings.py
+++ LearnIT/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'turno.ithranbeor7@gmail.com'
-EMAIL_HOST_PASSWORD = 'pvdfrqdvsfuozlol'
+EMAIL_HOST_USER = 'maeamorht@gmail.com'
+EMAIL_HOST_PASSWORD = 'kezfbgrsoccegemf'
 
 # Login/Logout Redirects
 LOGIN_URL = 'login'
